dfeabe568c28d9936c1443585e4af939ec907ed0
byrmaxi <42945875+byrmaxi@users.noreply.github.com>
2018-10-20 10:23:00 +0300

    Update dvymernieMas.cpp

--- dvymernieMas.cpp
+++ dvymernieMas.cpp
@@ -4,44 +4,34 @@ using namespace std;
 
 int main( ) 
 {
-    int m,n,stolb0;
-    cout << "Введите размер матрицы \n";
-    cout << "строк - ";
-    cin >> m ;
-    cout << "столбцов - ";
-    cin >> n;
-    int matr[m][n];    // как бы массив динамический, но все работает  
+    int const m=3, n=5;
     
-    cout << "Заполните матрицу \n" ;
-    for (int i=0; i<m; i++)
-      {
-      	for (int j=0; j<n; j++) 
-      	  {
-      	  	cout << " matr[" << i << ";" << j << "] = ";
-      	  	cin >> matr[i][j];
-      	  }  
-      }
-    
-    stolb0=0;
+    int matr[m][n]=
+     {
+         {2,2,0,1,3},
+         {2,0,0,2,6},
+         {4,4,4,5,1},
+     };    
+     
+    int stolb0=0;
     for (int i=0; i<n; i++)
       {
       	for (int j=0; j<m; j++) 
       	  {
       	      if (matr[j][i]==0) 
-		{
-		   stolb0++;
-		   break;
-		}
+	        {
+		  stolb0++;
+		  break;
+	        }
       	  }
       }
      cout << stolb0 << " - количесвто столбцов, где есть хотя бы один нулевой элемент" << endl; 
      
-     int max_odinakov=0,odinakov,iodinakov;
+     int max_odinakov=0,odinakov=0,iodinakov=0;
      for (int i=0; i<m; i++)
        {
        	for (int j=0; j<(n-1); j++)
        	  {
-       	  	odinakov=0;
        	  	if (matr[i][j]==matr[i][j+1]) odinakov++;
        	  }
        	if  (odinakov>max_odinakov)
@@ -49,8 +39,9 @@ int main( )
        	  	max_odinakov=odinakov;
        	  	iodinakov=i;
        	  }
+       	odinakov=0;
        }
-     cout <<  " В строке под номером " << iodinakov << " находится самая длинная серия одинаковых элементов";  
+     cout <<  " В строке под номером " << iodinakov+1 << " находится самая длинная серия одинаковых элементов";  
     
 	return 0;
 }
